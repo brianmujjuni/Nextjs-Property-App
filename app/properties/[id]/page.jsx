45d8f34eee7c4234970fe44bbc108441fdce30bc
brianmujjuni <brianmujjuni@engineer.com>
2024-12-09 17:07:10 +0300

    create Property header image component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,3 +1,12 @@
-export default function PropertyPage({ params }) {
-  return <div>PropertyPage {params.id}</div>;
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+export default async function PropertyPage({ params }) {
+  await connectDB();
+  const property = await Property.findById(params.id);
+  return (
+    <>
+      <PropertyHeaderImage image={property?.images[0]} />
+    </>
+  );
 }
--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,8 +1,14 @@
-import properties from "@/properties.json";
+// import properties from "@/properties.json";
 import PropertyCard from "./PropertyCard";
 import Link from "next/link";
-export default function HomeProperties() {
-  const recentProperties = properties.slice(0, 3);
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+export default async function HomeProperties() {
+  await connectDB();
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
   return (
     <>
       <section className="px-4 py-4">
@@ -10,7 +16,7 @@ export default function HomeProperties() {
           <h2 className="text-3xl font-bold text-blue-500 mb-5 text-center">
             Recent Properties
           </h2>
-          {properties.length === 0 ? (
+          {recentProperties.length === 0 ? (
             <p>No properties found</p>
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -22,7 +28,10 @@ export default function HomeProperties() {
         </div>
       </section>
       <section className="m-auto max-w-lg my-6 px-6">
-        <Link href="/properties" className="block bg-black text-white text-center py-4 px-6 rounded-xl">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl"
+        >
           View All Properties
         </Link>
       </section>
