0dce3ab95c17509d7894a20444d8e96f67ded1c2
brianmujjuni <brianmujjuni@engineer.com>
2024-12-14 12:49:24 +0300

    create property details component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "@/assets/images/logo.png";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-gray-200 py-4 mt-auto">
+    <footer className="bg-gray-200 py-4 mt-80">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
           <Image src={Logo} alt="propery-logo" className="h-8 w-auto" />
--- components/PropertiesDetails.jsx
+++ components/PropertiesDetails.jsx
@@ -1,105 +1,112 @@
 export default function PropertiesDetails({ property }) {
   return (
     <main>
-      <div class="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-        <div class="text-gray-500 mb-4">Apartment</div>
-        <h1 class="text-3xl font-bold mb-4">Boston Commons Retreat</h1>
-        <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-          <i class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-          <p class="text-orange-700">120 Tremont Street Boston, MA 02111</p>
+      <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+        <div className="text-gray-500 mb-4">Apartment</div>
+        <h1 className="text-3xl font-bold mb-4">Boston Commons Retreat</h1>
+        <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+          <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+          <p className="text-orange-700">120 Tremont Street Boston, MA 02111</p>
         </div>
-        <h3 class="text-lg font-bold my-6 bg-gray-800 text-white p-2">
+        <h3 className="text-lg font-bold my-6 bg-gray-800 text-white p-2">
           Rates & Options
         </h3>
-        <div class="flex flex-col md:flex-row justify-around">
-          <div class="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-            <div class="text-gray-500 mr-2 font-bold">Nightly</div>
-            <div class="text-2xl font-bold">
-              <i class="fa fa-xmark text-red-700"></i>
+        <div className="flex flex-col md:flex-row justify-around">
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Nightly</div>
+            <div className="text-2xl font-bold">
+              <i className="fa fa-xmark text-red-700"></i>
             </div>
           </div>
-          <div class="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-            <div class="text-gray-500 mr-2 font-bold">Weekly</div>
-            <div class="text-2xl font-bold text-blue-500">$1,100</div>
+          <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Weekly</div>
+            <div className="text-2xl font-bold text-blue-500">$1,100</div>
           </div>
-          <div class="flex items-center justify-center mb-4 pb-4 md:pb-0">
-            <div class="text-gray-500 mr-2 font-bold">Monthly</div>
-            <div class="text-2xl font-bold text-blue-500">$4,200</div>
+          <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
+            <div className="text-gray-500 mr-2 font-bold">Monthly</div>
+            <div className="text-2xl font-bold text-blue-500">$4,200</div>
           </div>
         </div>
       </div>
-      <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-        <h3 class="text-lg font-bold mb-6">Description & Details</h3>
-        <div class="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <h3 className="text-lg font-bold mb-6">Description & Details</h3>
+        <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
           <p>
-            <i class="fa-solid fa-bed"></i> 3
-            <span class="hidden sm:inline">Beds</span>
+            <i className="fa-solid fa-bed"></i> 3
+            <span className="hidden sm:inline">Beds</span>
           </p>
           <p>
-            <i class="fa-solid fa-bath"></i> 2
-            <span class="hidden sm:inline">Baths</span>
+            <i className="fa-solid fa-bath"></i> 2
+            <span className="hidden sm:inline">Baths</span>
           </p>
           <p>
-            <i class="fa-solid fa-ruler-combined"></i>
-            1,500 <span class="hidden sm:inline">sqft</span>
+            <i className="fa-solid fa-ruler-combined"></i>
+            1,500 <span className="hidden sm:inline">sqft</span>
           </p>
         </div>
-        <p class="text-gray-500 mb-4">
+        <p className="text-gray-500 mb-4">
           This is a beautiful apartment located near the commons
         </p>
       </div>
-      <div class="bg-white p-6 rounded-lg shadow-md mt-6">
-        <h3 class="text-lg font-bold mb-6">Amenities</h3>
-        <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+        <h3 className="text-lg font-bold mb-6">Amenities</h3>
+        <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Full kitchen
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Full
+            kitchen
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Washer & Dryer
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Washer &
+            Dryer
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Free Parking
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Free
+            Parking
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Hot Tub
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Hot Tub
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>24/7 Security
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>24/7
+            Security
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Wheelchair
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Wheelchair
             Accessible
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Elevator Access
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Elevator
+            Access
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Dishwasher
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Dishwasher
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Gym/Fitness
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Gym/Fitness
             Center
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Air
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Air
             Conditioning
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Balcony/Patio
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
+            Balcony/Patio
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Smart TV
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Smart TV
           </li>
           <li>
-            <i class="fas fa-check text-green-600 mr-2 mt-3"></i>Coffee Maker
+            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Coffee
+            Maker
           </li>
         </ul>
       </div>
       {/* <!-- Map --> */}
-      <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+      <div className="bg-white p-6 rounded-lg shadow-md mt-6">
         <div id="map"></div>
       </div>
     </main>
