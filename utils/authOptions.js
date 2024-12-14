ce39c1973cf32e672de5cf5d047b32716eb05ec3
brianmujjuni <brianmujjuni@engineer.com>
2024-12-14 15:39:32 +0300

    add authOptions utility

---
+++ utils/authOptions.js
@@ -0,0 +1,31 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          respone_type: "code",
+        },
+      },
+    },
+  ],
+  callback: {
+    //Invoked on successful signup
+    async signIn({ profile }) {
+      //conect to the database
+      //check if user exists
+      //if not create user
+      //return true to allow sign in
+    },
+    //Session callback function that modifies the session object
+    async session({session}){
+        // Get user from database
+        //Assign user id from the session
+    }
+  },
+};
