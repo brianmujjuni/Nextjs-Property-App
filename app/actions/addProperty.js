aa3e1b0d1b09ed32cd7a5a73b5b70d6465bb01c4
brianmujjuni <brianmujjuni@engineer.com>
2024-12-17 15:43:30 +0300

    create property in db

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,6 +1,18 @@
 "use server";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 async function addProperty(formData) {
+  await connectDB();
+  const sessionUser = await getSessionUser();
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+  const { userId } = sessionUser;
+
   const amenities = formData.getAll("amenities");
   const images = formData
     .getAll("images")
@@ -8,6 +20,7 @@ async function addProperty(formData) {
     .map((image) => image.name);
 
   const propertyData = {
+    owner: userId,
     type: formData.get("type"),
     name: formData.get("name"),
     description: formData.get("description"),
@@ -33,6 +46,10 @@ async function addProperty(formData) {
     },
     images,
   };
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
 }
 
 export default addProperty;
