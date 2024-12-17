1e8cae2ee8d4f1ead404aa568d805ae65fc7d446
brianmujjuni <brianmujjuni@engineer.com>
2024-12-17 14:37:27 +0300

    get property details from form in addPropperty action

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -339,7 +339,7 @@ export default function PropertyAddForm() {
         <input
           type="text"
           id="seller_name"
-          name="seller_info.name."
+          name="seller_info.name"
           className="border rounded w-full py-2 px-3"
           placeholder="Name"
         />
