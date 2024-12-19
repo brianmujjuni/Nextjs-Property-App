05507b16e06dcc850f197baa5f40d8acd07d8827
brianmujjuni <brianmujjuni@engineer.com>
2024-12-19 22:36:21 +0300

    resolve header image

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -6,7 +6,7 @@ export default function PropertyHeaderImage({ image }) {
       <div className="container-xl m-auto">
         <div className="grid grid-cols-1">
           <Image
-            src={`/images/properties/${image}`}
+            src={image}
             className="object-cover h-[400px] w-full"
             alt=""
             width={0}
